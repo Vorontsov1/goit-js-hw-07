8f13c84ff49782ba443405d51d6d6b1522186e34
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-09-10 22:03:46 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryInner = document.querySelector(".gallery");
+
+const galleryItem = createGalleryItem(galleryItems);
+
+galleryInner.insertAdjacentHTML("beforeend", galleryItem);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                    class="gallery__image"
+                    src=${preview}
+                    data-source=${original}
+                    alt=${description}
+                />
+            </a>
+        </div>`;
+    })
+    .join("");
+}
+
+galleryInner.addEventListener("click", galleryItemClick);
+
+function galleryItemClick(evt) {
+  evt.preventDefault();
+  const gallerySwatchEl = evt.target.dataset.source;
+  if (!gallerySwatchEl) {
+    return;
+  }
+  const lightBox = basicLightbox.create(`
+    <img src="${gallerySwatchEl}" width="800" height="600">
+`);
+  lightBox.show();
+
+  document.addEventListener("keydown", onClickEsc);
+
+  function onClickEsc(evt) {
+    if (evt.code !== "Escape") {
+      return;
+    }
+    lightBox.close();
+    document.removeEventListener("keydown", onClickEsc);
+  }
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryInner = document.querySelector(".gallery");
+
+const galleryItem = createGalleryItem(galleryItems);
+
+galleryInner.insertAdjacentHTML("beforeend", galleryItem);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" 
+   src="${preview}" 
+   alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionsDelay: 250,
+});
